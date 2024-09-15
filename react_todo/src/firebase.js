8344e78effec6f03daf0040cf15f04ab6835dde2
Your Name <you@example.com>
2024-09-15 22:37:55 +0900

    firebase初期化

---
+++ react_todo/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAHl7WWt-LBvQnbp-owJj3vS8LbgET5878",
+  authDomain: "mens-todos-8b5a4.firebaseapp.com",
+  projectId: "mens-todos-8b5a4",
+  storageBucket: "mens-todos-8b5a4.appspot.com",
+  messagingSenderId: "528250001085",
+  appId: "1:528250001085:web:a0abda5b754a19a04ac9d4",
+  measurementId: "G-DQMQEYGG93"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
